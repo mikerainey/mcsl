d6df9199fc38e5a03b695290f107e0362710d985
mikerainey <me@mike-rainey.site>
2020-03-15 20:23:04 -0400

    adding cmdline dependency

--- example/shell.nix
+++ example/shell.nix
@@ -2,7 +2,13 @@
   stdenv ? pkgs.stdenv,
   gcc ? pkgs.gcc,
   hwloc ? pkgs.hwloc,
-  jemalloc ? pkgs.jemalloc450 # use jemalloc, unless this parameter equals null (for now, use v4.5.0, because 5.1.0 has a deadlock bug)
+  jemalloc ? pkgs.jemalloc450, # use jemalloc, unless this parameter equals null (for now, use v4.5.0, because 5.1.0 has a deadlock bug)
+  cmdlineSrc ? pkgs.fetchFromGitHub {
+    owner  = "deepsea-inria";
+    repo   = "cmdline";
+    rev    = "c5f96b4aecb2019b5a690176195d37f7df3ed34b";
+    sha256 = "1rz9bfdd5242gy3vq4n9vj2rcr5pwp0j4cjycpn3pm7rnwrrcjnh";
+  }
 }:
 
 stdenv.mkDerivation rec {
@@ -22,12 +28,14 @@ stdenv.mkDerivation rec {
         else
           "export PATH=${jemalloc}/bin:$PATH";
     in
+    let cmdline = import "${cmdlineSrc}/script/default.nix" {}; in
     ''
     export CPP="${gcc}/bin/g++"
     export CC="${gcc}/bin/gcc"
     export MCSL_INCLUDE_PATH="../include/"
     export HWLOC_INCLUDE_PREFIX="-DMCSL_HAVE_HWLOC -I${hwloc.dev}/include/"
     export HWLOC_LIBRARY_PREFIX="-L${hwloc.lib}/lib/ -lhwloc"
+    export CMDLINE_INCLUDE_PATH="${cmdline}/include"
   '';
   
 }

0b04e9b3dc9b9453342f8e8f8c5ce5aa481fe2cc
Mike Rainey <me@mike-rainey.site>
2020-05-26 23:35:47 -0400

    switching to better hash function

--- include/mcsl_util.hpp
+++ include/mcsl_util.hpp
@@ -12,12 +12,18 @@ namespace mcsl {
 /* Hash function */
 
 using hash_value_type = uint64_t;
-  
-hash_value_type hash(hash_value_type x) {
-  x = (x ^ (x >> 30)) * UINT64_C(0xbf58476d1ce4e5b9);
-  x = (x ^ (x >> 27)) * UINT64_C(0x94d049bb133111eb);
-  x = x ^ (x >> 31);
-  return x;
+
+inline
+hash_value_type hash(hash_value_type u) {
+  uint64_t v = u * 3935559000370003845ul + 2691343689449507681ul;
+  v ^= v >> 21;
+  v ^= v << 37;
+  v ^= v >>  4;
+  v *= 4768777513237032717ul;
+  v ^= v << 20;
+  v ^= v >> 41;
+  v ^= v <<  5;
+  return v;
 }
 
 /*---------------------------------------------------------------------*/
